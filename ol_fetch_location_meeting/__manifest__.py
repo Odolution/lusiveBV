e1f8af92d2a0011246aa4d2da3487018c32bb7a7
Huzaifa Ahmed <44926606+huxe@users.noreply.github.com>
2022-12-19 15:48:04 +0500

    location

---
+++ ol_fetch_location_meeting/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ol Fetch Location in Meeting",
+
+    'summary': """""",
+
+    'description': """
+    """,
+
+    'author': "Asir Amin",
+    'website': "www.odolution.com",
+    'category': 'Specific Industry Applications',
+    'version': '0.1',
+    'depends': ['base', 'sale', 'crm'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'license': 'AGPL-3',
+}
