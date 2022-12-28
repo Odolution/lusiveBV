b93c64590a3659374028c2bda30c67c5e5304c26
Anique <aniquekhan004@gmail.com>
2022-12-28 17:28:44 +0500

    Bug Fix
    Bug Fix

--- ol_select_vendor_so/models/purchase_price_field.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api, _
-
-class Inheritssaleorder(models.Model):
-    _inherit = 'sale.order'
-
-    purchase_price = fields.Integer(string='Purchase Price', compute="_compute_purchase_price")
-
-    def _compute_purchase_price(self):
-        pass
