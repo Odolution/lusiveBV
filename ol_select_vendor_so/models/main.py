604892fbfa1bac2b57b19b746338d577d32eab69
Anique <aniquekhan004@gmail.com>
2022-12-30 12:06:07 +0500

    Update main.py

--- ol_select_vendor_so/models/main.py
+++ ol_select_vendor_so/models/main.py
@@ -24,64 +24,64 @@ class Inheritssaleorder(models.Model):
     def action_confirm(self):
         # raise UserError(str("Custom Purchase Order Method is being called"))
  
-        for i in self.order_line:
+        # for i in self.order_line:
 
-            product = self.env['product.product'].search([('id', '=', i.product_id.id)])
+        #     product = self.env['product.product'].search([('id', '=', i.product_id.id)])
 
 
-            temp = []
-            for j in product.seller_ids:
-                temp.append(j.name.name)
+        #     temp = []
+        #     for j in product.seller_ids:
+        #         temp.append(j.name.name)
 
-            if i.vendor_id and i.vendor_id.name not in temp:
-                temp2 = {
-                    "name": i.vendor_id.id,
-                    "product_uom": 'Unit',
-                    "price": 0.00,
-                    "delay": 1,
-                    "sequence": 1
-                }
-                product.seller_ids = [(0, 0, temp2)]
+        #     if i.vendor_id and i.vendor_id.name not in temp:
+        #         temp2 = {
+        #             "name": i.vendor_id.id,
+        #             "product_uom": 'Unit',
+        #             "price": 0.00,
+        #             "delay": 1,
+        #             "sequence": 1
+        #         }
+        #         product.seller_ids = [(0, 0, temp2)]
 
-                temp.append(i.vendor_id.name)
+        #         temp.append(i.vendor_id.name)
 
-            for j in product.seller_ids:
-                if j.name.name == i.vendor_id.name:
-                    j.sequence = 1
-                else:
-                    j.sequence += 1
+        #     for j in product.seller_ids:
+        #         if j.name.name == i.vendor_id.name:
+        #             j.sequence = 1
+        #         else:
+        #             j.sequence += 1
 
 
         # Custom Purchase Order
+        for rec in self:
+            unique_vendors = []
+            vendorPOdict={}
+            for i in rec.order_line:
+                if i.vendor_id not in unique_vendors and i.product_id.auto_purchase:
+                    unique_vendors.append(i.vendor_id)
+            for vendor in unique_vendors:
+                data2={}
+                data2['partner_id']= vendor.id
+                data2['SO_id']=self.id
+                po = self.env['purchase.order'].create(data2)
+                vendorPOdict[vendor]=po
 
-        unique_vendors = []
-        vendorPOdict={}
-        for i in self.order_line:
-            if i.vendor_id not in unique_vendors and i.product_id.auto_purchase:
-                unique_vendors.append(i.vendor_id)
-        for vendor in unique_vendors:
-            data2={}
-            data2['partner_id']= vendor.id
-            data2['SO_id']=self.id
-            po = self.env['purchase.order'].create(data2)
-            vendorPOdict[vendor]=po
+            data={}
 
-        data={}
-
-        for line in self.order_line:
-            if line.product_id.auto_purchase:
-                po=vendorPOdict[line.vendor_id]
-                data['url_pr'] = line.url
-                data['item_price'] = line.item_price
-                data['lead_time'] = line.leadtime
-                data['product_id'] = line.product_id.id
-                data['name'] = line.name
-                data['product_qty'] = line.product_uom_qty
-                data['price_unit'] = line.price_unit
-                data['order_id']=po.id
-        
-                pol=self.env['purchase.order.line'].create(data)
-        raise UserError(str(data))
+            for line in rec.order_line:
+                if line.product_id.auto_purchase:
+                    po=vendorPOdict[line.vendor_id]
+                    data['url_pr'] = line.url
+                    data['item_price'] = line.item_price
+                    data['lead_time'] = line.leadtime
+                    data['product_id'] = line.product_id.id
+                    data['name'] = line.name
+                    data['product_qty'] = line.product_uom_qty
+                    data['price_unit'] = line.price_unit
+                    data['order_id']=po.id
+            
+                    pol=self.env['purchase.order.line'].create(data)
+            raise UserError(str(data))
         
 
         # res=super(Inheritssaleorder, self).action_confirm()
